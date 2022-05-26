6980eb5b9db5f39d624d12145ba76e6d6a83dc43
HEE-JE <windba78@naver.com>
2022-05-26 11:17:24 +0900

    user - login #2

--- mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.douzone.mysite.repository;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.douzone.mysite.vo.UserVo;
@@ -33,6 +34,55 @@ public class UserRepository {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} finally {
 			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public UserVo findByEmailAndPassword(UserVo vo) {
+		UserVo result = null;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			connection = getConnection();
+
+			// 3. SQL 준비
+			String sql = "select no, name from user where email=? and password=?";
+			pstmt = connection.prepareStatement(sql);
+
+			// 4. Parameter Mapping
+			pstmt.setString(1, vo.getEmail());
+			pstmt.setString(2, vo.getPassword());
+
+			// 5. SQL 실행
+			rs = pstmt.executeQuery();
+
+			// 6. 결과처리
+			if (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+
+				result = new UserVo();
+				vo.setNo(no);
+				vo.setName(name);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
 				if (pstmt != null) {
 					pstmt.close();
 				}
