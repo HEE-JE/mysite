a384511d5207be6a52ce3041e16aea3374f894db
HEE-JE <windba78@naver.com>
2022-05-26 16:38:43 +0900

    user sql

--- sql/user.sql
+++ sql/user.sql
@@ -5,3 +5,15 @@ desc user;
 insert into user values(null, '관리자', 'admin@mysite.com', '1234', 'male', now());
 
 select * from user;
+
+-- findByEmailAndPassword
+select no, name from user where email='windba78@naver.com' and password='q15975302Q!';
+
+-- findByNo
+select no, name, email, gender from user where no=1;
+
+-- updateWithPassword
+update user set name='또치', password='1111', gender='female' where no=4;
+
+-- updateWithoutPassword
+update user set name='또치', gender='female' where no=4;
