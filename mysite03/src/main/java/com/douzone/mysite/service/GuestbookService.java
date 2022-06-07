d87bdb418f9a11f613b95f53ea838dfc97410cd7
HEE-JE <windba78@naver.com>
2022-06-07 15:49:13 +0900

    mysite03 - guestbook #1

---
+++ mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
@@ -0,0 +1,22 @@
+package com.douzone.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.douzone.mysite.vo.GuestbookVo;
+
+@Service
+public class GuestbookService {
+	public List<GuestbookVo> getMessageList() {
+
+	}
+
+	public Boolean deleteMessage(GuestbookVo vo) {
+
+	}
+
+	public Boolean addMessage(GuestbookVo vo) {
+
+	}
+}
