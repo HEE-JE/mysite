96654c8481fec0622cda518a3af3a256e1979ca7
HEE-JE <windba78@naver.com>
2022-06-08 16:20:50 +0900

    mybatis #2

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -1,12 +1,5 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,159 +8,23 @@ import com.douzone.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
+
 	@Autowired
 	private SqlSession sqlSession;
-	@Autowired
-	private DataSource dataSource;
 
 	public boolean insert(UserVo vo) {
 		return sqlSession.insert("user.insert", vo) == 1;
 	}
 
 	public UserVo findByEmailAndPassword(UserVo vo) {
-		UserVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			connection = dataSource.getConnection();
-
-			// 3. SQL 준비
-			String sql = "select no, name from user where email=? and password=?";
-			pstmt = connection.prepareStatement(sql);
-
-			// 4. Parameter Mapping
-			pstmt.setString(1, vo.getEmail());
-			pstmt.setString(2, vo.getPassword());
-
-			// 5. SQL 실행
-			rs = pstmt.executeQuery();
-
-			// 6. 결과처리
-			if (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-			}
-
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return sqlSession.selectOne("user.findByEmailAndPassword", vo);
 	}
 
-	public UserVo findByNo(Long no) {
-		UserVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			connection = dataSource.getConnection();
-
-			// 3. SQL 준비
-			String sql = "select no, name, email, gender from user where no=?";
-			pstmt = connection.prepareStatement(sql);
-
-			// 4. Parameter Mapping
-			pstmt.setLong(1, no);
-
-			// 5. SQL 실행
-			rs = pstmt.executeQuery();
-
-			// 6. 결과처리
-			if (rs.next()) {
-				String name = rs.getString(2);
-				String email = rs.getString(3);
-				String gender = rs.getString(4);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-				result.setEmail(email);
-				result.setGender(gender);
-			}
-
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+	public UserVo findByNo(Long userNo) {
+		return sqlSession.selectOne("user.findByNo", userNo);
 	}
 
 	public boolean update(UserVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		try {
-			connection = dataSource.getConnection();
-
-			if ("".equals(vo.getPassword())) {
-				// 3. SQL 준비
-				String sql = "update user set name=?, gender=? where no=?";
-				pstmt = connection.prepareStatement(sql);
-
-				// 4. Parameter Mapping(binding)
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getGender());
-				pstmt.setLong(3, vo.getNo());
-			} else {
-				String sql = "update user set name=?, password=?, gender=? where no=?";
-				pstmt = connection.prepareStatement(sql);
-
-				// 4. Parameter Mapping(binding)
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getPassword());
-				pstmt.setString(3, vo.getGender());
-				pstmt.setLong(4, vo.getNo());
-			}
-			// 5. SQL 실행
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return sqlSession.update("user.update", vo) == 1;
 	}
 }
